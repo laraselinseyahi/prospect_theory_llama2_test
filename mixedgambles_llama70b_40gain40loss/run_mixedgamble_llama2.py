e247798bcd754417d2477a59f4f491322e6388a9
Russell Poldrack <poldrack@gmail.com>
2024-02-24 11:48:33 -0800

    fix prompt - telling to only use one word caused bizzare behavior

--- mixedgambles_llama70b_40gain40loss/run_mixedgamble_llama2.py
+++ mixedgambles_llama70b_40gain40loss/run_mixedgamble_llama2.py
@@ -12,10 +12,10 @@ from pt_utils import read_api_key
 # task parameters
 min_gain = 0
 max_gain = 40
-gain_step = 1
+gain_step = 2
 min_loss = 0
 max_loss = 40
-loss_step = 1
+loss_step = 2
 
 
 # settings
@@ -32,7 +32,7 @@ temperature = 0
 
 system_prompt = "You are a human subject in a psychology experiment. " 
 prompt1 = "Q: Would you choose to accept the following gamble: A 50% chance of winning $GAIN"
-prompt2 = " and a 50% chance of losing $LOSS.  Please respond with only the word Yes or No. "
+prompt2 = " and a 50% chance of losing $LOSS.  Please respond with 'yes' or 'no'"
 start = "You have $100 in your pocket. "
 
 modelstring = model.split("/")[-1].split('-')[-2]
@@ -47,8 +47,8 @@ responses = []
 
 for gain in np.arange(min_gain, max_gain + gain_step, gain_step):
     for loss in np.arange(min_loss, max_loss + loss_step, loss_step):
-        p1 = prompt1.replace("GAIN", f"${gain}", 1)  # Updating gain
-        p2 = prompt2.replace("LOSS", f"${loss}", 1)  # Updating loss
+        p1 = prompt1.replace("GAIN", f"{gain}", 1)  # Updating gain
+        p2 = prompt2.replace("LOSS", f"{loss}", 1)  # Updating loss
         full_prompt = start + p1 + p2
 
         chat_completion = client.chat.completions.create(
@@ -68,7 +68,10 @@ for gain in np.arange(min_gain, max_gain + gain_step, gain_step):
         )
         print(full_prompt)
         print("gain:", gain, " loss:", loss, chat_completion.choices[0].message.content )
-        responses.append({"gain": str(gain), "loss": str(loss), 'response': chat_completion.choices[0].message.content})
+        responses.append({"prompt": full_prompt, 
+                          "gain": str(gain), 
+                          "loss": str(loss), 
+                          'response': chat_completion.choices[0].message.content})
 
 # Write responses to a JSON file
 # Use a separate key for metadata and responses
@@ -93,5 +96,5 @@ with open(f'{outfile_stem}.json', 'w') as json_file:
 df = pd.DataFrame(output['responses'])
 df['gain'] = df['gain'].astype(int)
 df['loss'] = df['loss'].astype(int)
-df['response_int'] = [1 if i.strip().lower().find('yes') == 0 else 0 for i in df['response']]
+df['response_int'] = [1 if i.strip().lower().find('yes') > -1 else 0 for i in df['response']]
 df.to_csv(f'{outfile_stem}.csv', index=False)
